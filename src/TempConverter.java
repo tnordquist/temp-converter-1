a7734f24c5c57fce529eb029218d01b36428a693
Todd A Nordquist <toddnordquist144@gmail.com>
2019-01-23 14:32:23 -0700

    Renamed class.

--- src/TempConverter.java
+++ src/TempConverter.java
@@ -1,7 +1,6 @@
 import java.util.*;
-import java.lang.Math;
 
-public class JavaApplication {
+public class TempConverter {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
